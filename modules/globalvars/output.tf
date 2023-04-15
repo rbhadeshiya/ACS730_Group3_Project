401f0417bd5c96dec6197126a44181ae00c41443
nklpawar <126423100+nklpawar@users.noreply.github.com>
2023-04-15 00:23:02 -0400

    Add files via upload

---
+++ modules/globalvars/output.tf
@@ -0,0 +1,17 @@
+
+
+# Default tags
+output "default_tags" {
+  value = {
+    "App"   = "Two-Tier Web Application automation with Terraform"
+    "Onwer" = "Rushi Bhadeshiya, Meet Parmar, Nikhil Pawar"
+  }
+}
+
+
+
+
+# Prefix to for resources
+output "prefix" {
+  value     = "Group3"
+}
